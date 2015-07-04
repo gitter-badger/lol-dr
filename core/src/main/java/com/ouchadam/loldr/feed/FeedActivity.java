d092a1f05d857c328d9254e9efea1d39afc895b9
Adam Brown <adam@novoda.com>
2015-07-04 13:04:41 +0100

    extracting the post marshalling to a marshaller factory

--- core/src/main/java/com/ouchadam/loldr/feed/FeedActivity.java
+++ core/src/main/java/com/ouchadam/loldr/feed/FeedActivity.java
@@ -1,6 +1,7 @@
 package com.ouchadam.loldr.feed;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.ouchadam.auth.Token;
@@ -9,7 +10,6 @@ import com.ouchadam.auth.UserTokenRequest;
 import com.ouchadam.loldr.BaseActivity;
 import com.ouchadam.loldr.data.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import rx.Subscriber;
@@ -20,75 +20,43 @@ public class FeedActivity extends BaseActivity {
 
     private TokenProvider tokenProvider;
     private Presenter presenter;
+    private MarshallerFactory marshallerFactory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        tokenProvider = TokenProvider.newInstance();
-        presenter = Presenter.onCreate(this);
+        this.marshallerFactory = new MarshallerFactory();
+        this.tokenProvider = TokenProvider.newInstance();
+        this.presenter = Presenter.onCreate(this);
 
         Repository.newInstance(provider).frontPage()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Repository.Feed>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(Repository.Feed feed) {
-                        Toast.makeText(FeedActivity.this, "" + feed.getPosts().size(), Toast.LENGTH_LONG).show();
-
-                        List<Post> uiPosts = marshallToUi(feed);
-
-                        presenter.present(uiPosts);
-
-                    }
-                });
-
-        // TODO: presenter.present(List<Posts>)
+                .subscribe(presentResult());
     }
 
-    private List<Post> marshallToUi(Repository.Feed feed) {
-        List<Repository.Post> dataPosts = feed.getPosts();
-        List<Post> posts = new ArrayList<>(dataPosts.size());
+    private Subscriber<Repository.Feed> presentResult() {
+        return new Subscriber<Repository.Feed>() {
+            @Override
+            public void onCompleted() {
+                // do nothing
+            }
 
-        for (final Repository.Post dataPost : dataPosts) {
-            posts.add(new Post() {
-                @Override
-                public String getId() {
-                    return dataPost.getId();
-                }
+            @Override
+            public void onError(Throwable e) {
+                Log.e("!!!", "Something went wrong", e);
+            }
 
-                @Override
-                public String getTitle() {
-                    return dataPost.getTitle();
-                }
+            @Override
+            public void onNext(Repository.Feed feed) {
+                Toast.makeText(FeedActivity.this, "" + feed.getPosts().size(), Toast.LENGTH_LONG).show();
 
-                @Override
-                public String getTime() {
-                    return String.valueOf(dataPost.getCreatedUtcTimeStamp());
-                }
+                List<Post> uiPosts = marshallerFactory.posts().marshall(feed.getPosts());
 
-                @Override
-                public String getSubreddit() {
-                    return dataPost.getSubreddit();
-                }
+                presenter.present(uiPosts);
 
-                @Override
-                public String getCommentCount() {
-                    return String.valueOf(dataPost.getCommentCount());
-                }
-            });
-        }
-
-        return posts;
+            }
+        };
     }
 
     private Repository.TokenProvider provider = new Repository.TokenProvider() {
